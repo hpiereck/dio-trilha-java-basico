f943bbb62c0d49e972f9ce65c8d562621d32e1ca
Henrique Piereck <hpiereck@gmail.com>
2025-08-22 13:03:08 -0300

    new commit

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        String meuNome = "Henrique";
+        
+        double salarioMinimo = 2500.00;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; // esse cast torna um numero de precisao maior em um de precisão menor
+
+        int numero = 1;
+
+        numero = 2;
+
+        final double PI = 3.14;
+
+//       PI = 10.75; //final não pode ser alterado pois é uma constante
+
+        
     }
 }
