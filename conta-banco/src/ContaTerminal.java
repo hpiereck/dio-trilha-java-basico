38c12f90fdb2f5580ee58e33ef12812035a8bcea
Henrique Piereck <hpiereck@gmail.com>
2025-08-22 14:57:30 -0300

    desafio sendo feito, falta tratar excecao

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o seu número da conta");
+        int numero = scanner.nextInt();
+
+        System.out.println("Digite o número da agência");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o seu nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite o seu saldo atual");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero
+        + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        scanner.close();
+
+        //TODO: Conhecer e importar a classe Scanner
+
+        // exibir as msg pro usuario
+
+        // obter pela scanner os valores digitados no terminal
+
+        // exibir a msg conta criada
     }
 }
