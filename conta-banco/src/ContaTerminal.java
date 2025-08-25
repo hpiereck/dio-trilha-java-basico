7bfa2ef51d87efc6a7dacfed8810a172caa269ee
Henrique Piereck <hpiereck@gmail.com>
2025-08-25 10:40:50 -0300

    exc feito

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,8 +1,9 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
         System.out.println("Digite o seu número da conta");
         int numero = scanner.nextInt();
 
@@ -12,10 +13,13 @@ public class ContaTerminal {
         System.out.println("Digite o seu nome");
         String nome = scanner.next();
 
+        System.out.println("Digite o seu sobrenome");
+        String sobrenome = scanner.next();
+
         System.out.println("Digite o seu saldo atual");
         double saldo = scanner.nextDouble();
 
-        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero
+        System.out.println("Olá " + nome +  " " + sobrenome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero
         + " e seu saldo " + saldo + " já está disponível para saque.");
 
         scanner.close();
